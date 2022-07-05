6f17de3fa082dc8d875034927a85d82e907c13f8
Roberto Rodrigues Filho <r.rodriguesfilho@lancaster.ac.uk>
2022-07-05 20:29:57 -0300

    update

---
+++ remove_docker_images.sh
@@ -0,0 +1,7 @@
+docker rmi server-ctl:latest
+docker rmi robertovrf/server-ctl:latest
+docker rmi remote-dist:latest
+docker rmi robertovrf/remote-dist:latest
+docker rmi distributor:latest
+docker rmi python:3
+docker rmi ubuntu:18.10
